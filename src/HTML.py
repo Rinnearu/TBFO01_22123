a76a9dd4fc2c4966d93411f7d982f4ca4002bcad
Jimly <untuktugasajaoke@gmail.com>
2023-11-26 21:59:39 +0700

    new rev

--- src/HTML.py
+++ src/HTML.py
@@ -11,70 +11,43 @@ class HTML:
                 data = file.read()
 
             temp = ""
-            in_comment = False
-            head_flag = False
-            body_flag = False
-            flag_close = False  # Menginisialisasi flag_close
+            outside_text = ""
             for char in data:
-                char = char.replace('\n', '')
-                char = char.replace('\t', '')
-                char = char.strip()
                 if char == '<':
-                    # buat tag comment
-                    if temp and not in_comment and not flag_close:
-                    # rayhan request input gajelas itu dijadiin T
+                    if outside_text.strip():  # Jika ada teks di luar tag
                         self.content.append('T')
-                    # bilamana ada teks tak ber tag di head atau body
-                    elif temp and (head_flag or body_flag):
-                    # rayhan request input gajelas itu dijadiin T
-                        self.content.append('T')
-                    head_flag = False
-                    body_flag = False
+                        outside_text = ""
                     temp = char
-                    flag_close = False  # Reset flag ketika menemui '<'
-
                 elif char == '>':
                     temp += char
-                    if temp.startswith('<!--') and temp.endswith('-->'):
-                        self.content.append('<!--')
-                        self.content.append('-->')
-                        temp = ""
-                    else:
-                        if temp == '<head>':
-                            head_flag = True
-                            self.content.append(temp[:-1])
-                            self.content.append(temp[-1])
-                        elif temp == '<body>':
-                            body_flag = True
-                            self.content.append(temp[:-1])
-                            self.content.append(temp[-1])
-                        elif temp.startswith('<link'):
-                            self._process_link_tag(temp)
-                        else:
-                            self.content.append(temp[:-1])
-                            self.content.append(temp[-1])
-                        temp = ""
-                    flag_close = True  # Set flag ketika menemui '>'
+                    self._process_tag(temp)
+                    temp = ""
                 else:
-                    temp += char
-            
-            # kalau ada string di luar html
-            if temp and not in_comment: #and not flag_close
+                    if temp:
+                        temp += char
+                    else:
+                        outside_text += char
+
+            if outside_text.strip():  # Menangani teks yang tersisa
                 self.content.append('T')
 
         except FileNotFoundError:
             print("File tidak ditemukan. Pastikan path file sudah benar.")
 
-    def _process_link_tag(self, tag):
-        # Ekstrak atribut dari tag <link>
-        attributes = re.findall(r'(\w+)="([^"]*)"', tag)
-        link_data = ['<link']
-        for attr, value in attributes:
-            link_data.append(attr + '="')
-            link_data.append('"')
-            # link_data.append(value + '"')
-        link_data.append('>')
-        self.content.extend(link_data)
+    def _process_tag(self, tag):
+        if tag.startswith('<!--') and tag.endswith('-->'):
+            self.content.append('<!--')
+            self.content.append('-->')
+        else:
+            parts = tag[:-1].split(' ', 1)
+            self.content.append(parts[0])
+            if len(parts) > 1:
+                attributes = re.findall(r'(\w+)="([^"]*)"', parts[1])
+                for attr, value in attributes:
+                    self.content.append(attr + '="')
+                    self.content.append('T ')  # Mengosongkan nilai atribut
+                    self.content.append('"')
+            self.content.append('>')
 
     def get_content(self):
         return self.content
